41675c2d253f7a000c3ba869197c285992e43bc7
flunoah <dragonsuny0305@gmail.com>
2026-06-02 19:32:07 +0900

    Create HealthController.java

---
+++ src/main/java/com/oliveyoung/mate/presentation/HealthController.java
@@ -0,0 +1,16 @@
+// src/main/java/com/oliveyoung/mate/presentation/HealthController.java
+
+package com.oliveyoung.mate.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
